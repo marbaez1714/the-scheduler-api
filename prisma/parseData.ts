8f54887a11d7b22a33474a340211778cede38d2e
Mario <marbaez1714@gmail.com>
2022-12-07 15:28:16 -0600

    added new seed scripts

---
+++ prisma/parseData.ts
@@ -0,0 +1,32 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+/******************************/
+/* Utils                      */
+/******************************/
+const parseData = async (type: string) => {
+  const raw = await fs
+    .readFile(path.resolve(__dirname, `./oldData/${type}.txt`), {
+      encoding: 'utf8',
+    })
+    .then((data) => data.split('\n').map((line) => JSON.parse(line)));
+
+  await fs.writeFile(
+    path.resolve(__dirname, `./oldData/${type}.json`),
+    JSON.stringify(raw)
+  );
+};
+
+export const getData = async () => {
+  await parseData('areas');
+  await parseData('builders');
+  await parseData('communities');
+  await parseData('companies');
+  await parseData('contractors');
+  await parseData('jobs');
+  await parseData('reporters');
+  await parseData('scopes');
+  await parseData('suppliers');
+};
+
+getData();
